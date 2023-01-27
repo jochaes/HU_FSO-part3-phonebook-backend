3f4bd408d519557833b0e7f4826d4a22812bcb2c
Josué Chaves Araya <d4n11083@estudiantec.cr>
2023-01-26 18:00:23 -0600

    3.7: Phonebook backend step7

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express =  require('express') //The aplication imports Node's built in web server module
+const morgan = require('morgan')
+
+
+
+
 const app = express()
 app.use(express.json())
-
+app.use(morgan('tiny'))
 
 let persons = [
   { 
