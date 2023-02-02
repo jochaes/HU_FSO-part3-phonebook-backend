82bc6b12b9e43010ba75998fbec820a36c5e7c5f
Josué Chaves Araya <d4n11083@estudiantec.cr>
2023-02-02 17:35:58 -0600

    3.15: Phonebook database step 3

--- index.js
+++ index.js
@@ -30,29 +30,6 @@ const unknownEndpoint = (request, response) => {
 
 app.use(express.json())
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
 
 app.get('/', 
   (request, response)=>{
@@ -95,12 +72,14 @@ app.get('/api/persons/:id', (request, response)=>{
 })
 
 app.delete('/api/persons/:id', (request, response)=>{
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id != id)
 
-  response.statusMessage = `Person with id ${id} deleted from database`
-  response.status(204).end()
-
+  Person.findByIdAndRemove( request.params.id)
+    .then( result => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      console.log(error.message);
+    })
 })
 
 const generateId = () => {
